b2f0c13a35ace1fe04f2c824198b6b4d60ea5bb9
Walter Marcel Barbosa Garcia <waltermbgarcia@hotmail.com>
2019-08-25 15:58:55 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function multiply() {
+  let multiply1 = document.querySelector("input#multiply1");
+  let multiply2 = document.querySelector("input#multiply2");
+  let result = document.querySelector("div.modal-body");
+
+  if (multiply1.value == "" || multiply2.value == "") {
+    result.innerHTML = "Woops...Seems like you forgot to type a number!";
+    $("#multiplyModal").on("hidden.bs.modal", function() {
+      $("#multiply1").focus();
+    });
+  } else {
+    let multiply1Value = Number(multiply1.value);
+    let multiply2Value = Number(multiply2.value);
+    result.innerHTML = "";
+
+    let totalMultiply = `${multiply1Value} x ${multiply2Value} = ${multiply1Value *
+      multiply2Value}`;
+
+    result.innerHTML = totalMultiply;
+  }
+}
